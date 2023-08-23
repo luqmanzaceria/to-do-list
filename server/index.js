7d7ebd86a267772e5e80b7ab6a3aecc8d0bcd4b5
Luqman Zaceria <luqmanzaceria@gmail.com>
2023-08-23 17:01:57 +0530

    Add endpoint for getting all tasks

--- server/index.js
+++ server/index.js
@@ -11,6 +11,16 @@ app.use(express.json()) // allows us to access the req.body
 
 //get all todos
 
+app.get("/todos", async (req, res) => {
+    try {
+        const allTodos = await pool.query("SELECT * FROM todo")
+
+        res.json(allTodos.rows);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
 //get a todo
 
 //create a todo
