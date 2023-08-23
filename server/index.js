cda043f7b4f72189c9797cec19e68bb3d63d75a6
Luqman Zaceria <luqmanzaceria@gmail.com>
2023-08-23 13:44:01 +0530

    Add task creation endpoint to REST API

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
-const express = require('express');
+const express = require('express')
 const app = express();
-const cors = require('cors');
+const cors = require('cors')
 const pool = require("./db")
 
 //middleware
-app.use(cors());
+app.use(cors())
 app.use(express.json()) // allows us to access the req.body
 
 //ROUTES//
@@ -14,6 +14,20 @@ app.use(express.json()) // allows us to access the req.body
 //get a todo
 
 //create a todo
+
+app.post("/todos", async (req,res) => {
+    try {
+        const { description } = req.body;
+        const newTodo = await pool.query(
+            "INSERT INTO todo (description) VALUES ($1) RETURNING *", 
+            [description]
+        );
+        
+        res.json(newTodo.rows[0]);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
 
 //update a todo
 
