0a5b6ca2117af2e769959e9ec1dde1ea487b382f
Luqman Zaceria <luqmanzaceria@gmail.com>
2023-08-23 17:27:29 +0530

    Add endpoint for getting a task by id

--- server/index.js
+++ server/index.js
@@ -23,6 +23,16 @@ app.get("/todos", async (req, res) => {
 
 //get a todo
 
+app.get("/todos/:id", async (req, res) => {
+    try {
+        const {id} = req.params;
+        const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [id]);
+        res.json(todo.rows[0]);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
 //create a todo
 
 app.post("/todos", async (req,res) => {
